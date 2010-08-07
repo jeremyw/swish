56b810107a26e3f3b8802d3602b49172b430e08a
Jeffrey Chupp <jeff@semanticart.com>
2010-08-07 10:04:48 -0400

    adding draftees, followers, and following associations to Player.  Adding twitter_screen_name and drafted_by_player_id as attributes on Player.

--- lib/dribbble/player.rb
+++ lib/dribbble/player.rb
@@ -1,6 +1,6 @@
 module Dribbble
   class Player < Base
-    attr_accessor :id, :name, :url, :avatar_url, :location,
+    attr_accessor :id, :name, :url, :avatar_url, :location, :twitter_screen_name, :drafted_by_player_id,
         :shots_count, :draftees_count, :followers_count, :following_count
 
     def self.find(id)
@@ -16,6 +16,18 @@ module Dribbble
     # Fetches shots by players that this player follows.
     def shots_following(options={})
       Shot.paginated_list(self.class.get("/players/#{@id}/shots/following", :query => options))
+    end
+
+    def draftees(options={})
+      Player.paginated_list(self.class.get("/players/#{@id}/draftees", :query => options), 'players')
+    end
+
+    def followers(options={})
+      Player.paginated_list(self.class.get("/players/#{@id}/followers", :query => options), 'players')
+    end
+
+    def following(options={})
+      Player.paginated_list(self.class.get("/players/#{@id}/following", :query => options), 'players')
     end
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -8,6 +8,8 @@ class PlayerTest < Test::Unit::TestCase
     assert_equal 'http://dribbble.com/players/simplebits', player.url
     assert_equal 'Salem, MA', player.location
     assert_not_nil player.created_at
+    assert_equal 'simplebits', player.twitter_screen_name
+    assert_nil player.drafted_by_player_id # no one drafted Dan
 
     assert_kind_of Numeric, player.shots_count
     assert_kind_of Numeric, player.draftees_count
@@ -47,5 +49,41 @@ class PlayerTest < Test::Unit::TestCase
     shots.each do |shot|
       assert_not_equal player, shot.player
     end
+  end
+
+  def test_draftees
+    player = Dribbble::Player.find(1)
+    draftees = player.draftees
+    assert_equal 15, draftees.size, "default page size"
+    draftees.each do |draftee|
+      assert_kind_of Dribbble::Player, draftee
+    end
+
+    draftees = player.draftees(:per_page => 2)
+    assert_equal 2, draftees.size
+  end
+
+  def test_followers
+    player = Dribbble::Player.find(1)
+    followers = player.followers
+    assert_equal 15, followers.size, "default page size"
+    followers.each do |follower|
+      assert_kind_of Dribbble::Player, follower
+    end
+
+    followers = player.followers(:per_page => 2)
+    assert_equal 2, followers.size
+  end
+
+  def test_following
+    player = Dribbble::Player.find(1)
+    following = player.following
+    assert_equal 15, following.size, "default page size"
+    following.each do |following|
+      assert_kind_of Dribbble::Player, following
+    end
+
+    following = player.following(:per_page => 2)
+    assert_equal 2, following.size
   end
 end
