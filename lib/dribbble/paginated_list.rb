7361529de3b630bd88269b42e18388fe4e5ef312
Jon Distad <jon.distad@gmail.com>
2010-09-26 23:53:35 -0400

    add Dribbble::PaginatedList#inspect

--- lib/dribbble/paginated_list.rb
+++ lib/dribbble/paginated_list.rb
@@ -6,8 +6,13 @@ module Dribbble
       @pages = response['pages']
       @per_page = response['per_page']
       @page = response['page']
-      @collection_class = Dribbble.const_get(collection_type[0..-2].capitalize)
-      super(response[collection_type].map { |attrs| @collection_class.new attrs })
+      collection_class = Dribbble.const_get(collection_type[0..-2].capitalize)
+      super(response[collection_type].map { |attrs| collection_class.new attrs })
+    end
+
+    def inspect
+      ivar_str = instance_variables.map {|iv| "#{iv}=#{instance_variable_get(iv).inspect}"}.join(", ")
+      "#<#{self.class.inspect} #{ivar_str}, @contents=#{super}>"
     end
   end
 end
